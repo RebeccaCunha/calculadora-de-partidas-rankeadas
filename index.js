dba92afeede58ccc2400e95eaf04cbe6948d67ad
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-10-24 10:43:11 -0300

    desafio-3

---
+++ index.js
@@ -0,0 +1,28 @@
+let resultado = vitorias(100, 30)
+let nome = "Finn";
+console.log(`O Herói ${nome}, tem o saldo de ${resultado.saldoRankeadas} e está o nível de ${resultado.nivel}!`)
+
+function vitorias (num1, num2) {
+    let saldoRankeadas = num1 - num2
+    let nivel = " ";
+
+
+    if (num1 < 10) {
+        nivel = "Ferro";
+    } else if (num1 > 11 && num1 < 20) {
+        nivel = "Bronze";
+    } else if (num1 > 21 && num1 < 50) {
+        nivel = "Prata";
+    } else if (num1 > 51 && num1 < 80) {
+        nivel = "Ouro";
+    } else if (num1 > 81 && num1 < 90) {
+        nivel = "Diamante";
+    } else if (num1 > 91 && num1 < 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+    
+    return {saldoRankeadas, nivel};
+}
+    
