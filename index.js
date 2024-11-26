58f14cbfef42b5b9e71b3e4308d845fe30e348b0
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-11-26 14:55:44 -0300

    correção

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let resultado = vitorias(100, 30)
+let resultado = vitorias(79, 50)
 let nome = "Finn";
 console.log(`O Herói ${nome}, tem o saldo de ${resultado.saldoRankeadas} e está o nível de ${resultado.nivel}!`)
 
@@ -9,15 +9,15 @@ function vitorias (num1, num2) {
 
     if (num1 < 10) {
         nivel = "Ferro";
-    } else if (num1 > 11 && num1 < 20) {
+    } else if (num1 >= 11 && num1 <= 20) {
         nivel = "Bronze";
-    } else if (num1 > 21 && num1 < 50) {
+    } else if (num1 >= 21 && num1 <= 50) {
         nivel = "Prata";
-    } else if (num1 > 51 && num1 < 80) {
+    } else if (num1 >= 51 && num1 <= 80) {
         nivel = "Ouro";
-    } else if (num1 > 81 && num1 < 90) {
+    } else if (num1 >= 81 && num1 <= 90) {
         nivel = "Diamante";
-    } else if (num1 > 91 && num1 < 100) {
+    } else if (num1 >= 91 && num1 <= 100) {
         nivel = "Lendário";
     } else {
         nivel = "Imortal";
